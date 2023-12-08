24e20330fb56c5b9268a8a8e1b5cb8162f68e95e
Alex-43 <82670395+alex-43@users.noreply.github.com>
2023-12-08 21:32:58 +0100

    day8, part 1

---
+++ day8/run.py
@@ -0,0 +1,27 @@
+
+
+with open('day8/files/data.txt') as f:
+    lines = f.readlines()
+    instruction_line = lines[0]
+    instructions = [0 if c == "L" else 1 for c in instruction_line.strip()]
+
+    node_map = {}
+    for line in lines[2:-1]:
+        node, pair = line.split("=")
+        pair = pair.replace("(", "")
+        pair = pair.replace(")", "")
+        lr = pair.split(",")
+        node_map[node.strip()] = [lr[0].strip(), lr[1].strip()]
+    #print (node_map)
+
+    steps = 0
+    instruction_index = 0
+    node = "AAA"
+    while node != "ZZZ":
+        instruction = instructions[instruction_index]
+        node = node_map[node][instruction]
+        instruction_index += 1
+        if instruction_index == len(instructions):
+            instruction_index = 0
+        steps += 1
+    print (steps)
