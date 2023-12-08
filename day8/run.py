8f96e0bf7e5b2a8d485dd59c0f3d26edbeed81fd
Alex-43 <82670395+alex-43@users.noreply.github.com>
2023-12-08 21:52:44 +0100

    corrected index error

--- day8/run.py
+++ day8/run.py
@@ -6,7 +6,7 @@ with open('day8/files/data.txt') as f:
     instructions = [0 if c == "L" else 1 for c in instruction_line.strip()]
 
     node_map = {}
-    for line in lines[2:-1]:
+    for line in lines[2:]:
         node, pair = line.split("=")
         pair = pair.replace("(", "")
         pair = pair.replace(")", "")
